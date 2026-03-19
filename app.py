b499a03ea1cb0bf6089c480a543eadf5f13fda6d
IVmarketing <267809883+IVmarketing@users.noreply.github.com>
2026-03-19 18:35:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,12 +36,7 @@ def get_model(model_type="flash"):
 model_flash = get_model("flash")
 model_pro = get_model("pro")
 
-safe_config = [
-    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
-    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
-    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
-    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
-]
+safe_config = None
 
 
 # ==========================================
